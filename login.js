a42b52fca8f0f6942e16642e343ac8d40097d780
Peter Kamenkovich <pkamenkovich@gmail.com>
2018-04-12 21:26:49 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,17 @@
+// Get the modal
+var modal = document.getElementById('id01');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+$(function() {
+    $("#loginForm").submit(function() {
+        var data = $(this).serialize().split("&");
+        alert(data);
+        return false;
+    });
+});
