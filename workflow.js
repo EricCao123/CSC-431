4b8bed70dc6c048e17fe1769183a1c485347988a
Peter Kamenkovich <pkamenkovich@gmail.com>
2018-05-06 15:13:01 -0400

    Update workflow.js

--- workflow.js
+++ workflow.js
@@ -18,13 +18,16 @@ $(function(){
         choice += $( this ).text();
       });
       //inside of each tr, look for the flag listed, and do something
+      var counter = 0;
       $('#pending > tbody > tr').each(function(){
+        counter ++;
         $(this).find('td').each(function() {
           if($(this).text() == choice){
             $(this).closest().css("background","blue");
           }
         });
       });
+      alert(counter);
     }
   );
 });
