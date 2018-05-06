e9085c0e48e22c7d150d1196d5684f663bc7e400
Peter Kamenkovich <pkamenkovich@gmail.com>
2018-05-06 10:18:24 -0400

    Update workflow.js

--- workflow.js
+++ workflow.js
@@ -7,10 +7,17 @@ $(function(){
 });
 
 $(function(){
-  //when .options changes, read the value.
+  
   $(".options").change(
+    //when .options changes
     function(){
-      alert($(this).text());
+      //run the function
+      var choices = "";
+      //for each selected value, read the selection and add them to the string choices
+      $( "select option:selected" ).each(function() {
+        choices += $( this ).text() + " ";
+      });
+      //split choices up, if need be, and read the values, only displaying tr's that have a 'flag' of the choices listed.
     }
   );
 });
