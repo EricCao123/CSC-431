c2580edc34b45024863a984a19148b5d9bc38f04
EricCao123 <38080531+EricCao123@users.noreply.github.com>
2018-05-06 20:22:18 -0400

    Update login.js

--- login.js
+++ login.js
@@ -11,10 +11,6 @@ window.onclick = function(event) {
 $(function() {
     $("#loginForm").submit(function() {
         var data = $(this).serialize().split("&");
-        //NEW CODE
-        var account = new Object();
-        account.username = data[0];
-        account.password = data[1];
         //Email, Password
         $.get( "ec2-18-220-98-102.us-east-2.compute.amazonaws.com/login.php", data , null )
             .done(function() {
